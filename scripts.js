d5376ccaf9f5f6b08ae421fa38d5e61939b419e6
Digi-Wulf <wulfhowlerstudios@gmail.com>
2022-12-19 13:52:03 -0600

    scripts

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav-bar").style.top = "0";
+    document.getElementById("nav-bar").style.opacity = "100";
+    document.getElementById("nav-bar").style.transition = ".5s";
+  } else {
+    document.getElementById("nav-bar").style.top = "-80px";
+    document.getElementById("nav-bar").style.opacity = "0";
+  }
+}
